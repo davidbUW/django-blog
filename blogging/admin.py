5065921fd4e2f72cb9ca1a9297dded6ead9614e4
David Burnett <dburnett2003@gmail.com>
2020-11-08 13:38:29 -0800

    assignment 7 - add ability to categorize while posting

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+
+class MembershipInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Fill me
+    inlines = [
+        MembershipInline,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Fill me
+    inlines = [
+        MembershipInline,
+    ]
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
